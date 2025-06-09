cd41ca24500553b7f7562c2c932e39484dc3a5b1
Katherine Sylvester <katsylvester88@gmail.com>
2025-06-09 14:16:13 +0000

    clean commit w/o secrets

---
+++ data_cleaning.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import json 
+import isodate 
+
+""" Cut down recipe df (if desired) and get number of and avg rating(s) """
+def get_slim_data(nrecipes=None): 
+
+    recipes = pd.read_parquet("data/recipes.parquet", engine="pyarrow")
+
+    print(f"Number of recipes: {recipes.shape}")
+    print("Recipe dataset columns: ")
+    print(recipes.columns)
+
+    # why not just take the most popular recipes
+    if nrecipes is not None: 
+        recipes_sorted = recipes.sort_values(
+            by=['ReviewCount', 'AggregatedRating'], 
+            ascending=[False, False])
+        recipes = recipes_sorted.head(nrecipes) 
+
+    # minor touchups 
+    recipes['ReviewCount'] = recipes['ReviewCount'].fillna(0)
+
+    return recipes
+
+"""
+Formatting goal: 
+{
+  "title": "Spicy Tofu Stir Fry",
+  "category": "Vegetable",
+  "description": "...",
+  "ingredients": ["tofu", "soy sauce", ...],
+  "steps": ["Cut tofu", "Heat oil", ...],
+  "tags": ["vegetarian", "quick", "asian"],
+  "total_time_in_mins": 30, 
+  "total_time_in_hours": 0.5, 
+  "num_reviews": 205, 
+  "average_rating": 4.25
+}
+"""
+def get_json_row(row): 
+    return {
+        "title": row['Name'], 
+        "category": row['RecipeCategory'],
+        "description": row['Description'], 
+        "ingredients": row['RecipeIngredientParts'].tolist(), 
+        "steps": row['RecipeInstructions'].tolist(), 
+        "tags": row['Keywords'].tolist(),
+        "total_time_in_mins": round(iso_duration_to_mins(row['TotalTime'])), 
+        "total_time_in_hours": round(iso_duration_to_hours(row['TotalTime']), 2), 
+        "num_reviews": int(row['ReviewCount']), 
+        "average_rating": round(row['AggregatedRating'], 2) 
+    }
+
+def iso_duration_to_mins(iso_duration):
+    """Converts an ISO 8601 duration string to minutes."""
+    duration = isodate.parse_duration(iso_duration)
+    total_seconds = duration.total_seconds()
+    return total_seconds / 60
+
+def iso_duration_to_hours(iso_duration):
+    """Converts an ISO 8601 duration string to minutes."""
+    duration = isodate.parse_duration(iso_duration)
+    total_seconds = duration.total_seconds()
+    return total_seconds / 3600
+
+if __name__ == '__main__': 
+    recipes = get_slim_data(500)
+
+    ex = get_json_row(recipes.iloc[0])
+    print("Example row: ", ex)
+
+    recipe_docs = [get_json_row(row) for _, row in recipes.iterrows()]
+
+    with open("data/formatted_recipes.json", "w") as f:
+        json.dump(recipe_docs, f)
+
+
---
+++ model.py
@@ -0,0 +1,124 @@
+import json
+import numpy as np 
+from sentence_transformers import SentenceTransformer
+import faiss 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch 
+
+""" Given a recipe, get an embedding that describes it for search """
+def get_recipe_emb(recipe, model):
+    title = recipe["title"]
+    category = recipe.get("category", "")
+    tags = ", ".join(recipe["tags"]) if recipe["tags"][0] else "" # some recipes don't have tags
+    desc = recipe.get("description", "")
+    ingredients = ", ".join(recipe["ingredients"])
+
+    search_text = f"{title} - {category} - {tags} - {desc} - {ingredients}"
+    return model.encode(search_text) 
+
+""" Given a recipe, get the context that will be returned if chosen by RAG """
+def get_recipe_context(recipe): 
+
+    title = recipe["title"]
+    tags = ", ".join(recipe["tags"]) if recipe["tags"][0] else ""
+    desc = recipe.get("description", "")
+    ingredients = ", ".join(recipe["ingredients"])
+    steps = ", ".join(recipe["steps"])
+
+    return f"Title: {title}\nDescription: {desc}\
+        \nTags: {tags}\nIngredients: {ingredients}\nSteps: {steps}"
+
+""" Given a model and dataset of recipes, save the recipes' IDs, 
+    context texts, and indexed search vectors (embeddings) """
+def save_embeddings(model, recipes_path="data/formatted_recipes.json"): 
+    
+    with open(recipes_path) as f:
+        recipes = json.load(f)
+
+    print(recipes[0])
+
+    recipe_ids = []
+    search_vecs = []
+    context_texts = []
+    for idx, recipe in enumerate(recipes): 
+        recipe_ids.append(idx)
+        search_vecs.append(get_recipe_emb(recipe, model))
+        context_texts.append(get_recipe_context(recipe))
+
+    emb_matrix = np.array(search_vecs).astype("float32")
+    index = faiss.IndexFlatL2(emb_matrix.shape[1])  # 384 dimensions
+    index.add(emb_matrix)
+    faiss.write_index(index, "recipe_index.faiss")
+
+    with open("data/context_texts.json", "w") as f:
+        json.dump(context_texts, f)
+
+    with open("data/recipe_ids.json", "w") as f:
+        json.dump(recipe_ids, f)
+
+def get_top_recipes(model, user_query): 
+    encoded_query = model.encode([user_query]) 
+
+    index = faiss.read_index("recipe_index.faiss")
+    D, I = index.search(np.array(encoded_query, dtype="float32"), k=3)
+
+    with open("data/context_texts.json") as f: 
+        context_texts = json.load(f) 
+    with open("data/formatted_recipes.json") as f: 
+        recipes = json.load(f) 
+
+    top_contexts = []
+    for i in I[0]: 
+        context = context_texts[i]
+
+        top_contexts.append(context)
+
+    return top_contexts
+
+def create_prompt(user_query, top_contexts): 
+
+    text = f"""You are a friendly cooking assistant that helps people find great recipes based on their needs.\n
+    A user has requested:\n
+    "{user_query}"\n
+    Here is one recipe you can consider: 
+    ===RECIPE START===\n
+    {top_contexts[0]}\n
+    ===RECIPE END===\n
+    Now write a helpful recommendation explaining why this recipe matches the user's request. Do not repeat the recipe formatting.\n
+    Response:
+    """
+    return text
+
+if __name__ == '__main__': 
+
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    user_query = "I want something with mushrooms. What do you recommend?"
+    
+    top_recipes = get_top_recipes(model, user_query)
+    
+    prompt = create_prompt(user_query, top_recipes)
+    print(prompt)
+
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    llm = AutoModelForCausalLM.from_pretrained("gpt2")
+
+    tokenizer.pad_token = tokenizer.eos_token 
+    llm.config.pad_token_id = tokenizer.eos_token_id
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+    output = llm.generate(
+        **inputs, 
+        max_new_tokens = 150, 
+        do_sample=True, 
+        temperature=0.8, 
+        top_p = 0.9, 
+        top_k=50,
+        repetition_penalty=1.2,
+        pad_token_id = tokenizer.eos_token_id
+    )
+
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    chatbot_response = generated_text[len(prompt):].strip()
+
+    print("Chatbot response: \n")
+    print(chatbot_response)
